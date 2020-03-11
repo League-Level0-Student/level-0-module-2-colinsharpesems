872cc21edfe92f5008dc4f7b599af7055652cb31
colinsharpesems <56978302+colinsharpesems@users.noreply.github.com>
2020-03-11 14:55:05 -0700

    GOOD JOB

--- src/_04_validation/Validation.java
+++ src/_04_validation/Validation.java
@@ -6,6 +6,7 @@ package _04_validation;
 import java.util.Random;
 
 import javax.swing.JOptionPane;
+import javax.swing.JPopupMenu;
 
 public class Validation {
 	public static void main(String[] args) {
@@ -17,9 +18,26 @@ public class Validation {
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
+	for (int i = 0; i <10; i++) {
+			
+		 randomNumber = randomMaker.nextInt(5);
+if (randomNumber ==0 ) {
+JOptionPane.showMessageDialog(null, "Ur cuul");	
+}
+if (randomNumber == 1) {
+	JOptionPane.showMessageDialog(null, "Ur awesome");	
+}
+if (randomNumber == 2) {
+	JOptionPane.showMessageDialog(null, "I like ur shirt.");	
+}
+if (randomNumber == 3) {
+	JOptionPane.showMessageDialog(null, "Nice shoes");	
+}
+if (randomNumber == 4) {
+	JOptionPane.showMessageDialog(null, "Ur good at coding.");	
+}
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
-	}
+		}}
 }
--- src/_05_magic_8_ball/Magic8Ball.java
+++ src/_05_magic_8_ball/Magic8Ball.java
@@ -9,7 +9,9 @@ import java.util.Random;
 public class Magic8Ball {
 
 	// 1. Make a main method that includes all the steps below….
+public static void main(String[] args) {
 
+}
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
 
 	// 3. Print out this variable
